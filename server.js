4c8e627f45b4bd5df293729f016e0a700d9841eb
Dominic Tremblay <dominictremblay24@gmail.com>
2020-03-16 21:55:43 -0400

    express server setup

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid/v4');
+
+const PORT = process.env.PORT || 3000;
+
+// creating an Express app
+const app = express();
+
+// morgan middleware allows to log the request in the terminal
+app.use(morgan('short'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Static assets (images, css files) are being served from the public folder
+app.use(express.static('public'));
+
+// Setting ejs as the template engine
+app.set('view engine', 'ejs');
+
+// In memory database
+const movieQuotesDb = {
+  d9424e04: {
+    id: 'd9424e04',
+    quote: 'Why so serious?',
+  },
+  '27b03e95': {
+    id: '27b03e95',
+    quote: 'YOU SHALL NOT PASS!',
+  },
+  '5b2cdbcb': {
+    id: '5b2cdbcb',
+    quote: "It's called a hustle, sweetheart.",
+  },
+  '917d445c': {
+    id: '917d445c',
+    quote: 'The greatest teacher, failure is.',
+  },
+  '4ad11feb': {
+    id: '4ad11feb',
+    quote: 'Speak Friend and Enter',
+  },
+};
+
+const quoteComments = {
+  '70fcf8bd': {
+    id: '70fcf8bd',
+    comment: 'So awesome comment!',
+    quoteId: 'd9424e04',
+  },
+};
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
