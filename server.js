aaac7a7ea8e79f447d6ed179fbc590d67894dd71
Dominic Tremblay <dominictremblay24@gmail.com>
2023-11-28 12:37:59 -0500

    final version

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 // morgan middleware allows to log the request in the terminal
-app.use(morgan('short'));
+app.use(morgan('dev'));
 
 // parse application/x-www-form-urlencoded => req.body
 // replacing bodyParser
@@ -43,15 +43,96 @@ const movieQuotesDb = {
   },
 };
 
-
 // END POINTS OR ROUTES
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the movie quotes app!')
-})
+  res.send('Welcome to the movie quotes app!');
+});
 
+app.get('/quotes.json', (req, res) => {
+  res.json(movieQuotesDb);
+});
 
 // CRUD Operations on quotes
 
+// LIST OF QUOTES
+// Get a list of quotes
+// READ
+// Path or the url: /quotes
+
+app.get('/quotes', (req, res) => {
+  // get the quotes from the db
+
+  // Pass it to the template
+  // if we don't => the view does not have access to it
+
+  // render the view => ejs = HTML + JS => compiled => HTML
+  res.render('quotes_list', {
+    title: 'List of Movie Quotes...',
+    movieQuotesList: movieQuotesDb,
+  }); // render => filename
+});
+
+// CREATE A NEW QUOTE
+
+// Display the form to add a new quote
+app.get('/quotes/new', (req, res) => {
+  res.render('new_quote');
+});
+
+// Process the form that it receives
+app.post('/quotes', (req, res) => {
+  // extract the quote content from the body of the request
+  console.log(req.body); // { quoteContent: 'Here’s Johnny!!!!' }
+
+  // We need to add the quote the db
+  // we need a new id
+  const id = Math.random().toString(36).substring(2, 8);
+
+  // add the new quote to the db
+  // nameOfObject[newKey] = value => {id: 123, quote: 'abc'}
+  movieQuotesDb[id] = {
+    id,
+    quote: req.body.quoteContent,
+  };
+
+  res.redirect('/quotes'); // ask the browser to perform another get /quotes
+});
+
+// UPDATE AN EXISTING QUOTE
+// Display an existing quote on the screen
+
+app.get('/quotes/:id', (req, res) => {
+  // extract the id from the path (url)
+  const id = req.params.id;
+
+  res.render('quote_show', { quoteContent: movieQuotesDb[id] });
+});
+
+// Update the quote
+app.post('/quotes/:id', (req, res) => {
+  // extract the id from the path
+  const id = req.params.id;
+
+  // we need to extract the updated quote
+  // bodyParser => req.body
+  const quoteContent = req.body.quoteContent;
+
+  // updating the quote in the db
+  movieQuotesDb[id].quote = quoteContent;
+
+  // redirect
+
+  res.redirect('/quotes');
+});
+
+app.post('/quotes/:id/delete', (req, res) => {
+  // extract the id from the path
+  const id = req.params.id;
+
+  delete movieQuotesDb[id];
+
+  res.redirect('/quotes');
+});
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
